74316c63e18d76a9b0fdf8d1251a33c324b4b83b
codeInquire-byte <muzakirjebi@gmail.com>
2025-12-19 17:28:16 -0500

    Fully Fleshed out AnimeCharacter superclass
    This superclass is meant to act as a hierarchical figure to the subclasses OnePiece and Jujutsu Kaisen.  It implements the Fighter and Comparable interfaces. From fighter, it generic abilites which all subclasses will also inherit, but in ways that affect their own uniqure instance variables. It also operates as a figure from which OnePiece and Jujutsukaisen will inherit behavior, such as attacking and combo attacking, as well as basic features that all subclasses will inherit like hp and attack power. it also has an equals, comparable, and tostring method which subclasses will call on using super. When using the default constructor, the character's stats are randomly created and a random character is retrievered from the arraylist instance variable called characters. When the custom constructor is called, if the character is not in the arraylist characters, a character is forcibly chosen for them.

--- AnimeCharacter.java
+++ AnimeCharacter.java
@@ -1,3 +1,124 @@
-public class AnimeCharacter {
+import java.util.*;
 
+public class AnimeCharacter implements Fighter, Comparable<AnimeCharacter> {
+    private int attackPower;
+    private int hp;
+    private String character;
+    private ArrayList<String> characters = new ArrayList<String>(Arrays.asList("Beerus", "Goku", "Ichigo", "Naruto", "Sasuke"));
+    ;
+
+    public AnimeCharacter() {
+        attackPower = (int)(Math.random() * 100) + 1;
+        hp = (int)(Math.random() * 51) + 50;
+        character = this.characters.get((int)(Math.random() * 5));
+    }
+
+    public AnimeCharacter(int attackPower, int hp, String character) {
+        if (!this.characters.contains(character)) {
+            this.character = "Goku";
+            this.attackPower = 100;
+            this.hp = 100;
+        } else {
+            this.character = character;
+            this.attackPower = attackPower;
+            this.hp = hp;
+        }
+    }
+
+    public void attack(AnimeCharacter other) {
+        System.out.println(this.character + " attacked " + other.character + " for " + 
+            (this.attackPower/10) + " damage! Their health went from " + other.hp + " to "
+            + (other.hp - (this.attackPower/10)) + ".");
+
+        other.hp -= this.attackPower/10;
+
+        if (other.getHp() < 0) {
+            System.out.println("The opponent's HP has fallen below 0, they are now dead.");
+        } else if (other.getHp() == 0) {
+            System.out.println("The opponent has died.");
+        }
+    }
+
+    public void comboAttack(AnimeCharacter other, int times) {
+        if (times == 0 ) {
+            return;
+        }
+        if (other.getHp() < 0) {
+            System.out.println("The opponent's HP has fallen below 0, they are now dead.");
+            return;
+        } else if (other.getHp() == 0) {
+            System.out.println("The opponent has died.");
+            return;
+        }
+        attack(other);
+        comboAttack(other, times - 1);
+    }
+
+    public void recover() {
+        this.hp += (int)(this.hp*0.2);
+
+    }
+
+    public void gameOver() {
+        this.attackPower = 0;
+        this.hp = 0;
+        this.character = null;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    @Override
+    public int compareTo(AnimeCharacter other) {
+        if (this.attackPower == other.attackPower) {
+            if (this.hp == other.hp) {
+                return 0;
+            } else {
+                return this.hp - other.hp;
+            }
+        }
+        else {
+            return this.attackPower - other.attackPower;
+        }
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o instanceof AnimeCharacter) {
+            AnimeCharacter other = (AnimeCharacter)o;
+            boolean result = (this.attackPower == other.attackPower) && (this.hp == other.getHp())
+            && (this.character.equals(other.getCharacter()));
+            return result;
+        }
+        return false;
+    }
+
+    public String toString() {
+        return this.character + " has an attack power of " + this.attackPower + " and " + this.hp + " health.";
+    }
 }
